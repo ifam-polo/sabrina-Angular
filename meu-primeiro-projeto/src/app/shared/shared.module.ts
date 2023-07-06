3b1685a1d17629b3c272f0e1345133b8d4138092
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2023-07-06 15:27:01 -0400

    comunicação entre componentes e serviços

--- meu-primeiro-projeto/src/app/shared/shared.module.ts
+++ meu-primeiro-projeto/src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodAddComponent } from './food-add/food-add.component';
 
 
 
@@ -10,9 +12,17 @@ import { OutputComponent } from './output/output.component';
   declarations: [
     NewComponentComponent,
     InputComponent,
-    OutputComponent
+    OutputComponent,
+    FoodListComponent,
+    FoodAddComponent
   ],
-  exports: [NewComponentComponent, InputComponent, OutputComponent],
+  exports: [
+    NewComponentComponent, 
+    InputComponent, 
+    OutputComponent, 
+    FoodListComponent,
+    FoodAddComponent],
+    
   imports: [
     CommonModule
   ]
