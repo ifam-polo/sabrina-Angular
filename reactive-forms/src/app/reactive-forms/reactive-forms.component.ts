7991c8e229f913b1657446549619c0c3dbd17866
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-07-09 23:12:24 -0400

    começando reactive forms

---
+++ reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+
+  }
+
+}
