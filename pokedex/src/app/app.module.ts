71dd0a1ec65f22987794d24588c027f7e036c7b9
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-07-11 19:34:44 -0400

    pegando informações da API para a listagem dos pokemons

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
-import { RoutingModule } from './pages/routing.module';
+
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { RoutingModule } from './pages/routing.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
