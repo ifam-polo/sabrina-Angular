8e95654032de9fa31513ec3637e8e7760b42734b
Sabrina Frazão <sabrinadasilvafrazao@gmail.com>
2023-07-14 18:34:09 -0400

    finalizando a criação do serviço insvestiment

--- TESTE-UNITARIO/src/app/shared/investiments/components/list/list.component.ts
+++ TESTE-UNITARIO/src/app/shared/investiments/components/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Investiments } from '../../model/investiments';
+import { ListInvestimentsService } from '../../services/list-investiments.service';
 
 @Component({
   selector: 'app-list',
@@ -30,8 +31,13 @@ export class ListComponent implements OnInit{
     },
 
   ];
-  constructor() {}
+  constructor(
+    private listInvestimentsService:ListInvestimentsService
+  ) {}
   ngOnInit(): void {
+    this.listInvestimentsService.list().subscribe(
+      res => console.log(res)
+    )
   }
 
 }
